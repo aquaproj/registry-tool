ca53a1919032a874fbc2c201436289788c402338
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-10-24 21:44:33 +0900

    fix: remove aqua-local.yaml (#464)
    * fix: remove aqua-local.yaml
    
    aqua-local.yaml is unnecessary anymore.
    We test packages in the container.
    
    * refactor: remove unused functions

--- pkg/scaffold/error.go
+++
@@ -1,9 +0,0 @@
-package scaffold
-
-import "errors"
-
-var (
-	errDocumentMustBeOne = errors.New("the number of document in aqua.yaml must be one")
-	errBodyFormat        = errors.New("fails to parse a configuration file. Format is wrong. body must be *ast.MappingNode or *ast.MappingValueNode")
-	errPkgsNotFound      = errors.New("the field 'packages' isn't found")
-)
