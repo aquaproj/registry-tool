3f10704d1534f57e10be993a599e78a96fbd7c63
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-11-13 09:17:36 +0900

    feat: check repo (#379)
    * feat: check repo
    
    * refactor(check-repo): refactor
    
    * fix: fix a lint error

---
+++ pkg/cli/check_repo.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"net/http"
+
+	"github.com/aquaproj/registry-tool/pkg/checkrepo"
+	"github.com/spf13/afero"
+	"github.com/urfave/cli/v2"
+)
+
+func (runner *Runner) newCheckRepoCommand() *cli.Command {
+	return &cli.Command{
+		Name:      "check-repo",
+		Usage:     `Check if GitHub Repository was transferred`,
+		UsageText: `$ aqua-registry check-repo <package name>`,
+		Description: `Check if GitHub Repository is transferred.
+This command succeeds if the repository isn't transferred.
+
+e.g.
+
+$ aqua-registry check-repo Azure/aztfy
+Azure/aztfexport
+`,
+		Action: runner.checkRepoAction,
+		Flags:  []cli.Flag{},
+	}
+}
+
+func (runner *Runner) checkRepoAction(c *cli.Context) error {
+	return checkrepo.CheckRepo( //nolint:wrapcheck
+		c.Context, afero.NewOsFs(), &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+		},
+		c.Args().First())
+}
