0ae103452e9e2197d918600a3b2c90cb49390859
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-11-14 14:55:47 +0900

    feat: change the signature of CheckRedirect (#500)
    * feat: change the signature of CheckRedirect
    
    * fix: fix the default package name
    
    * fix: fix the parse of location path

--- pkg/checkrepo/handler.go
+++ pkg/checkrepo/handler.go
@@ -29,62 +29,91 @@ type Alias struct {
 }
 
 func CheckRepo(ctx context.Context, afs afero.Fs, httpClient *http.Client, pkgName string) error {
-	repoOwner, repoName, err := CheckRedirect(ctx, afs, httpClient, pkgName)
+	redirect, err := CheckRedirect(ctx, afs, httpClient, pkgName)
 	if err != nil {
 		return err
 	}
+	if redirect == nil {
+		return nil
+	}
 
-	fmt.Printf("%s/%s\n", repoOwner, repoName)                                          //nolint:forbidigo
+	fmt.Printf("%s/%s\n", redirect.NewRepoOwner, redirect.NewRepoName)                  //nolint:forbidigo
 	return logerr.WithFields(errors.New("a repository was transferred"), logrus.Fields{ //nolint:wrapcheck
 		"package_name": pkgName,
-		"repo_owner":   repoOwner,
-		"repo_name":    repoName,
+		"repo_owner":   redirect.NewRepoOwner,
+		"repo_name":    redirect.NewRepoName,
 	})
 }
 
-func CheckRedirect(ctx context.Context, afs afero.Fs, httpClient *http.Client, pkgName string) (string, string, error) { //nolint:cyclop
+type Redirect struct {
+	RepoOwner      string
+	RepoName       string
+	NewRepoOwner   string
+	NewRepoName    string
+	NewPackageName string
+}
+
+func CheckRedirect(ctx context.Context, afs afero.Fs, httpClient *http.Client, pkgName string) (*Redirect, error) { //nolint:cyclop
 	registryPath := filepath.Join("pkgs", filepath.FromSlash(pkgName), "registry.yaml")
 	f, err := afero.ReadFile(afs, registryPath)
 	if err != nil {
-		return "", "", fmt.Errorf("open a registry file: %w", err)
+		return nil, fmt.Errorf("open a registry file: %w", err)
 	}
 	rgst := &Registry{}
 	if err := yaml.Unmarshal(f, rgst); err != nil {
-		return "", "", fmt.Errorf("decode a registry file as YAML: %w", err)
+		return nil, fmt.Errorf("decode a registry file as YAML: %w", err)
 	}
 	if len(rgst.Packages) != 1 {
-		return "", "", errors.New("the number of packages must be one")
+		return nil, errors.New("the number of packages must be one")
 	}
 	pkg := rgst.Packages[0]
 	if pkg.RepoOwner == "" || pkg.RepoName == "" {
-		return "", "", nil
+		return nil, nil //nolint:nilnil
 	}
 
 	resp, err := request(ctx, httpClient, pkg.RepoOwner, pkg.RepoName)
 	if err != nil {
-		return "", "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode < 300 { //nolint:gomnd,usestdlibvars
-		return "", "", nil
+		return nil, nil //nolint:nilnil
 	}
 	if resp.StatusCode >= 500 { //nolint:gomnd,usestdlibvars
-		return "", "", logerr.WithFields(errors.New("http status code >= 500"), logrus.Fields{ //nolint:wrapcheck
+		return nil, logerr.WithFields(errors.New("http status code >= 500"), logrus.Fields{ //nolint:wrapcheck
 			"http_status_code": resp.StatusCode,
 		})
 	}
 	if resp.StatusCode >= 400 { //nolint:gomnd,usestdlibvars
-		return "", "", logerr.WithFields(errors.New("http status code >= 400"), logrus.Fields{ //nolint:wrapcheck
+		return nil, logerr.WithFields(errors.New("http status code >= 400"), logrus.Fields{ //nolint:wrapcheck
 			"http_status_code": resp.StatusCode,
 		})
 	}
 
 	location, err := resp.Location()
 	if err != nil {
-		return "", "", fmt.Errorf("get location header: %w", err)
+		return nil, fmt.Errorf("get location header: %w", err)
 	}
-	return getRepoFromLocation(location.Path)
+	repoOwner, repoName, err := getRepoFromLocation(location.Path)
+	if err != nil {
+		return nil, err
+	}
+	redirect := &Redirect{
+		RepoOwner:      pkg.RepoOwner,
+		RepoName:       pkg.RepoName,
+		NewRepoOwner:   repoOwner,
+		NewRepoName:    repoName,
+		NewPackageName: pkgName,
+	}
+	repoFullName := fmt.Sprintf("%s/%s", pkg.RepoOwner, pkg.RepoName)
+	newRepoFullName := fmt.Sprintf("%s/%s", repoOwner, repoName)
+	if pkgName == repoFullName {
+		redirect.NewPackageName = newRepoFullName
+	} else if strings.HasPrefix(pkgName, repoFullName+"/") {
+		redirect.NewPackageName = strings.Replace(pkgName, repoFullName, newRepoFullName, 1)
+	}
+	return redirect, nil
 }
 
 func request(ctx context.Context, httpClient *http.Client, repoOwner, repoName string) (*http.Response, error) {
@@ -105,9 +134,10 @@ func getRepoFromLocation(location string) (string, string, error) {
 		return "", "", errors.New("location must start with /")
 	}
 
+	// /<repo_owner>/<repo_name>
 	b := strings.Split(location, "/")
-	if len(b) != 2 { //nolint:gomnd
-		return "", "", errors.New("location path format must be <repo_owner>/<repo_name>")
+	if len(b) != 3 { //nolint:gomnd
+		return "", "", errors.New("location path format must be /<repo_owner>/<repo_name>")
 	}
-	return b[0], b[1], nil
+	return b[1], b[2], nil
 }
