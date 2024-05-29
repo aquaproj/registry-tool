1c274db6e32b71b26087a0e9630c3a262c63ef45
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-05-29 19:06:07 +0900

    fix(create-pr-new-pkg): stop checkout branch and create a commit (#731)
    * fix(create-pr-new-pkg): stop checkout branch and create a commit
    
    * chore: fix a lint error

--- pkg/create-pr-new-pkg/api.go
+++ pkg/create-pr-new-pkg/api.go
@@ -18,7 +18,7 @@ import (
 //go:embed pr_template.md
 var bodyTemplate []byte
 
-func CreatePRNewPkgs(ctx context.Context, pkgNames ...string) error { //nolint:cyclop
+func CreatePRNewPkgs(ctx context.Context, pkgNames ...string) error {
 	if len(pkgNames) == 0 {
 		return errors.New(`usage: $ aqua-registry create-pr-new-pkg <pkgname>...
 e.g. $ aqua-registry create-pr-new-pkg cli/cli`)
@@ -43,7 +43,6 @@ e.g. $ aqua-registry create-pr-new-pkg cli/cli`)
 			return err
 		}
 	}
-	commitMsg := strings.Join(bodies, "\n")
 	body := strings.Join(append(bodies, []string{ //nolint:makezero
 		"",
 		"```console",
@@ -56,16 +55,6 @@ e.g. $ aqua-registry create-pr-new-pkg cli/cli`)
 	branch := "feat/" + pkgName
 	if err := initcmd.Init(ctx); err != nil {
 		return err //nolint:wrapcheck
-	}
-	if err := command(ctx, "git", "checkout", "-b", branch); err != nil {
-		return err
-	}
-	if err := command(ctx, "git", "commit", "-m", strings.Join([]string{
-		"feat: add " + pkgName,
-		"",
-		commitMsg,
-	}, "\n")); err != nil {
-		return err
 	}
 	if err := command(ctx, "git", "push", "origin", branch); err != nil {
 		return err
